a873fb1c116b260fd3a828ec887199b4bc30b40e
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2023-12-19 21:52:56 +0530

    Add files via upload

---
+++ 01_Basic.js
@@ -0,0 +1,21 @@
+
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+let accountCity = "Jaipur"
+let accountState;
+
+// accountId = 02 // Not allowed
+
+
+accountEmail ="ysk@gmail.com"
+accountPassword="54321"
+accountCity ="Patna"
+
+/*
+ Prefer not to  use -----> var varrible ----> It has no block scope
+  and functional scope  
+*/
+
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
