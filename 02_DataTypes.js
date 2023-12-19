d99988907d39ffc37eab3e42985951fa60306253
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2023-12-19 22:22:04 +0530

    Add files via upload

---
+++ 02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log("Sum is" + 3+3);
+console.log("Yash");
+
+
+// number => 2 to power 53
+
+let name = " Abc";
+
+let age = " 18"; 
+
+let isLoggedIn = false; 
+
+/* Primitivw Data Types
+
+   String data type
+  integer data type
+  boolean data type
+  null =>  standalone value
+  undefined => No value assign yet
+  symbol => unique
+
+*/
+
+console.log(typeof age);
+console.log(typeof null);
